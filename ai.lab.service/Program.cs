35bd07ad579f90721ca59246f10a63d4fb75ed1b
edwin hernandez gordillo <gordilloedwin@hotmail.com>
2025-09-26 14:33:55 -0600

    Initial commit: AI Lab worker setup

---
+++ ai.lab.service/Program.cs
@@ -0,0 +1,10 @@
+using ai.lab.service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
